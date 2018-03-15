4bcef72d484053580c3e5250b3971f2fc8a662c8
Pierre Rouanet <pierre.rouanet@gmail.com>
2018-03-15 12:53:10 +0100

    Add id and torque limit xl320 register.

--- src/motors/XL_320.rs
+++ src/motors/XL_320.rs
@@ -1,8 +1,10 @@
 //! Definition of the `XL_320` registers
 
 register![
+    ID: 0x03, 1,
+    TorqueEnable: 0x18, 1,
     PresentPosition: 0x25, 2,
     GoalPosition: 0x1E, 2,
     MovingSpeed: 0x20, 2,
-    TorqueEnable: 0x18, 1,
+    TorqueLimit: 0x23, 2,
 ];
